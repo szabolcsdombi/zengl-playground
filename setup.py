f0ba81d24d1925edacc08974ae32b8547cdb00a4
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-11-02 22:40:05 +0200

    0.2.1

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='zengl-playground',
+    version='0.2.1',
     packages=['zengl_playground', 'zengl_playground.renderers', 'zengl_playground.resources'],
     package_data={'zengl_playground.resources': ['**/*']},
     include_package_data=True,
