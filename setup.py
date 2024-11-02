6c065299a9b6f73af83c0ebe1f92ca8f33ebdcd4
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-11-02 21:54:30 +0200

    0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zengl-playground',
-    version='0.1.0',
+    version='0.2.0',
     packages=['zengl_playground'],
     install_requires=['zengl'],
 )
